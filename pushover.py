a152f40ec3ad69eaea76f54935061c374b068dbf
tiveritz <lukas.walch@icloud.com>
2021-04-11 09:25:47 +0200

    Refactor and implement emergency notification

--- pushover.py
+++ pushover.py
@@ -1,51 +1,58 @@
-import settings
-
-from enum import Enum
+import os
 import requests
 import json
-
-
-# TODO
-# - Beautify
-# - EMERGENCY requires additional post fields, not implemented yet
+from dotenv import load_dotenv
+from enum import Enum
 
 
 class Priority(Enum):
     LOWEST = -2     # does not trigger any sound or vibration
     LOW = -1        # does not trigger sound or vibration but display alert
-    NORMAL = 0      # trigger sound, vibration and display alert
-    HIGH = 1        # trigger sound, vibration and display alert, bypass quiet hours
+    NORMAL = 0      # triggers sound, vibration and display alert
+    HIGH = 1        # triggers sound, vibration and display alert, bypass quiet hours
     EMERGENCY = 2   # same as high, but repeatedly alert until acknowledged
 
-class PushoverRequest():
-    PUSHOVER_API_URL = settings.PUSHOVER_API_URL
-    PUSHOVER_USER_KEY = settings.PUSHOVER_USER_KEY
-    API_TOKEN = settings.API_TOKEN
-
-    def __init__(self, title, message, priority = Priority.NORMAL):
+class PushoverNotification():
+    def __init__(self, api_url, api_token, user_key):
+        self.api_url = api_url
+        self.api_token = api_token
+        self.user_key = user_key
+    
+    def prepare_request(self, title, message, priority = Priority.NORMAL):
         self.title = title
         self.message = message
         self.priority = priority
-    
+        self.emergency_retry = 600    # 10 Munutes
+        self.emergency_expire = 3600  # 1 Hour
+
     def post(self):
         payload = {
-            'token' : self.API_TOKEN,
-            'user' : self.PUSHOVER_USER_KEY,
+            'token' : self.api_token,
+            'user' : self.user_key,
             'title' : self.title,
             'message' : self.message,
             'priority' : self.priority.value
         }
 
-        if self.priority.value >= -2 and self.priority.value <= 1:
+        if self.priority == Priority.EMERGENCY:
+            payload['retry'] = self.emergency_retry
+            payload['expire'] = self.emergency_expire
 
-            r = requests.post(self.PUSHOVER_API_URL, data = payload)
-            self.status_code = r.status_code
-            self.text = r.text
-
-        # emergency requires retry and expire and is not handled yet
+        r = requests.post(self.api_url, data = payload)
+        self.status_code = r.status_code
+        self.text = r.text
 
 
-r = PushoverRequest("title", "message")
+# Imports settings from the .env file
+load_dotenv()
+API_URL = os.getenv("API_URL")
+API_TOKEN = os.getenv("API_TOKEN")
+USER_KEY = os.getenv("USER_KEY")
+
+# Sends the notification request to the API
+r = PushoverNotification(API_URL, API_TOKEN, USER_KEY)
+r.prepare_request("title", "message")
+#r.prepare_request("title", "message", Priority.EMERGENCY)
 r.post()
 
 print(r.status_code)
--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-PUSHOVER_API_URL = os.getenv("PUSHOVER_API_URL")
-PUSHOVER_USER_KEY = os.getenv("PUSHOVER_USER_KEY")
-API_TOKEN = os.getenv("API_TOKEN")

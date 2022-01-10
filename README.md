e619fba26f480078fae72f6b825a24f8c859727b
tiveritz <lukas.walch@icloud.com>
2022-01-10 22:30:54 +0100

    Improve code

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
-## Pushover Real-Time Notifications
+# Pushover Real-Time Notifications
 Pushover is a service to send and receive real-time notifications on
 Android, iPhone, iPad, and Desktop (Android Wear and Apple Watch, too!)
 It is very easy to use this service with the Pushover API.
-<br />
 
 API Documentation: [https://pushover.net/api](https://pushover.net/api)
 <br />
+<br />
 
-### Environment Variables
-Include the following environment variables into your .env file
-  * PUSHOVER_API_URL=https://api.pushover.net/1/messages.json
-  * PUSHOVER_USER_KEY -> You will find this key on the device app you want
-to send the notifications to
-  * API_TOKEN -> Login with your Pushover Account, go to Dashboard
-  https://pushover.net and create an Application/API Token
+## Usage
+```
+from pushover import PushoverNotification
+
+
+API_URL = 'https://api.pushover.net/1/messages.json
+API_TOKEN = # You will find this key on the device app you want to send the notifications to
+USER_KEY = # Login with your Pushover Account, go to Dashboard https://pushover.net and create an Application/API Token
+
+pushover = PushoverNotification(API_URL, API_TOKEN, USER_KEY)
+response = pushover.notify('title', 'message')
+
+print(response.status_code)
+print(response.text)
+```
+
+You can set the Priority like:
+```
+from pushover import Priority
+.
+.
+.
+r.prepare_request('title', 'message', Priority.EMERGENCY)
+```
+
+Available Priorities
+```
+LOWEST = -2     # does not trigger any sound or vibration
+LOW = -1        # does not trigger sound or vibration but display alert
+NORMAL = 0      # triggers sound, vibration and display alert
+HIGH = 1        # triggers sound, vibration and display alert, bypass quiet hours
+EMERGENCY = 2   # same as high, but repeatedly alert until acknowledged
+```
--- pushover.py
+++ pushover.py
@@ -1,58 +1,34 @@
-import os
 import requests
-from dotenv import load_dotenv
 from enum import Enum
 
 
 class Priority(Enum):
-    LOWEST = -2     # does not trigger any sound or vibration
-    LOW = -1        # does not trigger sound or vibration but display alert
-    NORMAL = 0      # triggers sound, vibration and display alert
-    HIGH = 1        # triggers sound, vibration and display alert, bypass quiet hours
-    EMERGENCY = 2   # same as high, but repeatedly alert until acknowledged
+    LOWEST = -2    # does not trigger any sound or vibration
+    LOW = -1       # does not trigger sound or vibration but display alert
+    NORMAL = 0     # triggers sound, vibration and display alert
+    HIGH = 1       # triggers sound, vibration and display alert, bypass quiet hours
+    EMERGENCY = 2  # same as high, but repeatedly alert until acknowledged
 
-class PushoverNotification():
+
+class PushoverNotification:
     def __init__(self, api_url, api_token, user_key):
         self.api_url = api_url
         self.api_token = api_token
         self.user_key = user_key
-    
-    def prepare_request(self, title, message, priority = Priority.NORMAL):
-        self.title = title
-        self.message = message
-        self.priority = priority
         self.emergency_retry = 600    # 10 Munutes
         self.emergency_expire = 3600  # 1 Hour
 
-    def post(self):
+    def notify(self, title, message, priority=Priority.NORMAL):
         payload = {
-            'token' : self.api_token,
-            'user' : self.user_key,
-            'title' : self.title,
-            'message' : self.message,
-            'priority' : self.priority.value
+            'token': self.api_token,
+            'user': self.user_key,
+            'title': title,
+            'message': message,
+            'priority': priority.value
         }
 
-        if self.priority == Priority.EMERGENCY:
+        if priority == Priority.EMERGENCY:
             payload['retry'] = self.emergency_retry
             payload['expire'] = self.emergency_expire
 
-        r = requests.post(self.api_url, data = payload)
-        self.status_code = r.status_code
-        self.text = r.text
-
-
-# Imports settings from the .env file
-load_dotenv()
-API_URL = os.getenv("API_URL")
-API_TOKEN = os.getenv("API_TOKEN")
-USER_KEY = os.getenv("USER_KEY")
-
-# Sends the notification request to the API
-r = PushoverNotification(API_URL, API_TOKEN, USER_KEY)
-r.prepare_request("title", "message")
-#r.prepare_request("title", "message", Priority.EMERGENCY)
-r.post()
-
-print(r.status_code)
-print(r.text)
+        return requests.post(self.api_url, data=payload)
